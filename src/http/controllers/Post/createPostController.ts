33321cff22d53d7646f324a8b8c074218731f45c
ItaloAraujodev <italoarauju2019@gmail.com>
2025-01-04 11:04:18 -0300

    refactor: remove unnecessary console logs in createPostController

--- src/http/controllers/Post/createPostController.ts
+++ src/http/controllers/Post/createPostController.ts
@@ -83,9 +83,7 @@ export async function createPostController(
           }),
         ) || [],
     };
-    console.log(postData);
-    const response = await makeCreatePost.execute(postData);
-    console.log(response);
+    await makeCreatePost.execute(postData);
     return reply.status(201).send({ message: "Post criado com sucesso." });
   } catch (error) {
     if (error instanceof z.ZodError) {
